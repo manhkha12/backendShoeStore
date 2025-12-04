9ecfe46b6b0bda7d65e73ac32adf74c27b4fbbe0
Kha Nguyen Manh <manhkhaquansi@gmail.com>
2025-12-04 21:01:06 +0700

    cc

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -60,8 +60,8 @@ exports.getOrder = async (req, res) => {
 exports.getUserOrders = async (req, res) => {
     const userId = req.user?.userId || req.query.user_id;
     try {
-        const orders = await orderService.getUserOrders(userId);
-        return successResponse(res, { orders }, "Lấy lịch sử đơn hàng thành công");
+        const data = await orderService.getUserOrders(userId);
+        return successResponse(res, data , "Lấy lịch sử đơn hàng thành công");
     } catch (err) {
         return errorResponse(res, err.message);
     }
